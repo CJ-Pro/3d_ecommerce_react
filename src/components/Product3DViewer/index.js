2005eff99fa3959f6fc688f1c07c6870d90f1022
CJ-Pro <cjn.pro96@gmail.com>
2021-01-06 21:44:57 +0000

    Add new asset

--- src/components/Product3DViewer/index.js
+++ src/components/Product3DViewer/index.js
@@ -27,8 +27,8 @@ export default function Product3DViewer() {
           direction={Vector3.Up()}
         />
         <Model
-          sceneFilename="scene.gltf"
-          rootUrl="https://github.com/CJ-Pro/3d_ecommerce_react/blob/main/src/assets/earring/"
+          sceneFilename="model.obj"
+          rootUrl="https://raw.githubusercontent.com/CJ-Pro/3d_ecommerce_react/main/public/assets/bracelet/"
           position={new Vector3(0, 0, 0)}
         />
       </Scene>
