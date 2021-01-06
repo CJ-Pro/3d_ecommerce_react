2c91c290a409d105a3250a7e4ee67b9d43c1310a
CJ-Pro <cjn.pro96@gmail.com>
2021-01-06 21:58:51 +0000

    Add tissot files

--- src/components/Product3DViewer/index.js
+++ src/components/Product3DViewer/index.js
@@ -28,7 +28,7 @@ export default function Product3DViewer() {
         />
         <Model
           sceneFilename="scene.gltf"
-          rootUrl="https://raw.githubusercontent.com/CJ-Pro/3d_ecommerce_react/main/public/assets/earring/"
+          rootUrl="https://raw.githubusercontent.com/CJ-Pro/3d_ecommerce_react/main/public/assets/tissot/"
           position={new Vector3(0, 0, 0)}
         />
       </Scene>
