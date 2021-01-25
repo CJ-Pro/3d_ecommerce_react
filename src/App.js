32161a35fc7b3ece8cb24febe3baf66b3743013f
CJ-Pro <cjn.pro96@gmail.com>
2021-01-25 11:36:30 +0000

    Add routes and pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MenuBar from "./components/MenuBar";
-import Product3dItem from "./components/Product3DItem";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import Product3DRepository from "./repository/Product3DRepository";
 
 function App() {
+  const product3Ddata = new Product3DRepository().getProducts();
   return (
-    <React.Fragment>
-      <MenuBar />
-      <Product3dItem />
-    </React.Fragment>
+    <Router>
+      <Switch>
+        {product3Ddata.map((product3d) => (
+          <Route exact path={`/product/${product3d.productId}`}>
+            <ProductPage product3d={product3d} />
+          </Route>
+        ))}
+        <Route exact path="/">
+          <MenuBar />
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
